7d5a29ffeec4af99cb8c7a1cefc2965fa506fc38
Tomasz Jarych <tjarych@gmail.com>
2018-10-04 16:48:32 +0200

    Ebook Manager Initial commit
    Signed-off-by: Tomasz Jarych <tjarych@gmail.com>

---
+++ src/main/java/tj/ebm/EbookManagerApplication.java
@@ -0,0 +1,12 @@
+package tj.ebm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbookManagerApplication.class, args);
+	}
+}
