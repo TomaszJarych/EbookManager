51496f2df26f99fdc3f4e270cb01a1ae02e36b74
Tomasz Jarych <tjarych@gmail.com>
2018-10-11 18:01:04 +0200

    Ebook Manager - Book page, booktable
    Signed-off-by: Tomasz Jarych <tjarych@gmail.com>

--- src/main/resources/static/js/homePage.js
+++ src/main/resources/static/js/homePage.js
@@ -1,47 +1,85 @@
+const booksTableDiv = $("#booksTable");
+const addNewBookButton = $("#addNewBookButton");
+const addNewBookForm = $("#addNewBookForm");
+const editBookForm = $("#editBookForm");
+
 $(document).ready(function () {
 
-    let dataFromDb = new BookFromDb("http://localhost:8080/ebm/book/all", "Get", "json");
-    
-    dataFromDb.fetch();
+    showBooksTable();
 
-    let testBooks = dataFromDb.books;
-    
-    
-    for (let index = 0; index < testBooks.length; index++) {
-        console.log(testBooks[index]);
-        
-    }
 
 
 });
 
-function BookFromDb(url, type, dataType) {
-    this.books = [];
-    this.url = url;
-    this.type = type;
-    this.dataType = dataType;
-}
+function showBooksTable() {
+    let output = "";
+    const tableHtmlBeginning = "<table id=\"table\" border=\"1\">";
+    const tableHtmlEnd = "</table>";
 
+    output += tableHtmlBeginning;
 
-BookFromDb.prototype.fetch = function () {
-    var that = this;
+    output += "<tr><th>Title</th><th>ISBN</th><th>Bookstore</th><th>Owner</th><th>Is Read?</th><th>Mark as Read</th><th>In Reader?</th><th>Is in Reader?</th></tr>";
 
     $.ajax({
-        url: that.url,
-        type: that.type,
-        dataType: that.dataType
+        url: "http://localhost:8080/ebm/book/all",
+        type: "GET",
+        dataType: "json"
     }).done(function (json) {
-            if (json["message"] === "OK") {
-
-                let booksFromJSON = json["data"];
-                for (let book of booksFromJSON) {
-                    
-                    that.books.push(book);
-                }
-            }
+        for (const book of json["data"]) {
+            output +=
+                "<tr><td>" + book.title + "</td>" +
+                "<td>" + book.isbn + "</td>" +
+                "<td>" + book.bookstore.name + "</td>" +
+                "<td>" + book.owner.firstName + " " + book.owner.lastName + "</td>" +
+                "<td>" + isRead(book.isRead) + "</td>" +
+                "<td>" + "<button id=\"markAsRead" + book.id + "\">" + getMarkAsReadButton(book.isRead) + "</button>" + "</td>" +
+                "<td>" + inReader(book.inReader) + "</td>" +
+                "<td>" + "<button id=\"markIsInReader" + book.id + "\">" + markIsInReader(book.inReader) + "</button>" + "</td>";
+            $("#markIsInReader" + book.id).on("click", {
+                id: book.id
+            }, function (event) {
+                console.log("MarkIsInReader" + event.data.id);
+            })
         }
 
-    ).fail(function () {
+
+        output += tableHtmlEnd;
+        booksTableDiv.append($(output));
+
+    }).fail(function () {
         alert("Cannot connect to server");
-    })
+
+    });
+
+    function isRead(isRead) {
+        if (isRead) {
+            return "Yes";
+        } else {
+            return "No";
+        }
+    }
+
+    function getMarkAsReadButton(isRead) {
+        if (isRead) {
+            return "Mark as Unread";
+        } else {
+            return "Mark as Read";
+        }
+    }
+
+    function inReader(inReader) {
+        if (inReader) {
+            return "Yes";
+        } else {
+            return "No";
+        }
+    }
+
+    function markIsInReader(inReader) {
+        if (inReader) {
+            return "Removed from reader";
+        } else {
+            return "Send to reader";
+        }
+    }
 }
