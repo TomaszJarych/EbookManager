bc60dd0a2cc9ad2d0910126159441ddb1153dc16
Tomasz Jarych <tjarych@gmail.com>
2019-01-24 11:15:51 +0100

    Bookstore converter tests

--- src/main/java/tj/ebm/Bookstore/domain/Bookstore.java
+++ src/main/java/tj/ebm/Bookstore/domain/Bookstore.java
@@ -1,14 +1,13 @@
 package tj.ebm.Bookstore.domain;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Entity
 @Table(name = "book_store")
 public class Bookstore {
@@ -23,4 +22,37 @@ public class Bookstore {
 
     private String web;
 
+    public static class BookstoreEntityBuilder {
+        private Long id;
+        private String name;
+        private String email;
+        private String web;
+
+        public BookstoreEntityBuilder() {
+        }
+
+        public BookstoreEntityBuilder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public BookstoreEntityBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public BookstoreEntityBuilder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public BookstoreEntityBuilder setWeb(String web) {
+            this.web = web;
+            return this;
+        }
+
+        public Bookstore build() {
+            return new Bookstore(id, name, email, web);
+        }
+    }
 }
--- src/main/java/tj/ebm/Bookstore/dto/BookstoreDto.java
+++ src/main/java/tj/ebm/Bookstore/dto/BookstoreDto.java
@@ -1,8 +1,6 @@
 package tj.ebm.Bookstore.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.Email;
@@ -11,6 +9,7 @@ import javax.validation.constraints.NotBlank;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class BookstoreDto {
 
     private Long id;
@@ -26,4 +25,37 @@ public class BookstoreDto {
     @URL
     private String web;
 
+    public static class BookstoreDtoBuilder {
+        private Long id;
+        private String name;
+        private String email;
+        private String web;
+
+        public BookstoreDtoBuilder() {
+        }
+
+        public BookstoreDtoBuilder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public BookstoreDtoBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public BookstoreDtoBuilder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public BookstoreDtoBuilder setWeb(String web) {
+            this.web = web;
+            return this;
+        }
+
+        public BookstoreDto build() {
+            return new BookstoreDto(id, name, email, web);
+        }
+    }
 }
